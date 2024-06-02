8e937f86b164cc4bcf3f91ecd87669daf297fee9
Rawley <ebotebot7@gmail.com>
2024-06-01 23:37:58 -0400

    Init Angular Structure

---
+++ Frontend/src/app/gigs/gig-detail/gig-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gig-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './gig-detail.component.html',
+  styleUrl: './gig-detail.component.css'
+})
+export class GigDetailComponent {
+
+}
---
+++ Frontend/src/app/gigs/gig-list/gig-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gig-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './gig-list.component.html',
+  styleUrl: './gig-list.component.css'
+})
+export class GigListComponent {
+
+}
